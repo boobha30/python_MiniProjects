4b3ee376fd05103e6289256a904d14ab6d069299
boobha30 <thiaguboobhashri@gmail.com>
2026-04-28 20:03:24 +0530

    Added Number Checker program

---
+++ Practice Code/NumberChecker.py
@@ -0,0 +1,12 @@
+number=int(input("Please enter an integer: "))
+if number%2==0:
+    print("Your number is even.")    
+elif number%2!=0:
+    print("Your number is odd.") 
+
+if number >0:
+    print("Your number is positive.")
+elif number <0:
+    print("Your number is negative.")
+else:    
+    print("Your number is zero.")
